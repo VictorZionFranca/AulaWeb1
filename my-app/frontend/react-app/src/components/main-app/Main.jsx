7fa9849501ba5dfe755ec7089729eee8b7b10dd3
Victor Zion França <vzion435@gmail.com>
2024-08-02 21:46:04 -0300

    inicio react

---
+++ my-app/frontend/react-app/src/components/main-app/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+import './Main.css'
+
+export default props => 
+    <React.Fragment>
+        <main className="content container-fluid">
+            <div className="p-3 mt-3">
+                
+            </div>
+        </main>
+    </React.Fragment>
